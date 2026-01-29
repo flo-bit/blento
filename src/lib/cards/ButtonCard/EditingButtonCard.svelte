7c3271b7e79a38c90db1b9a9f520e68fddc5435d
Florian <45694132+flo-bit@users.noreply.github.com>
2026-01-29 23:15:48 +0100

    improve button design

--- src/lib/cards/ButtonCard/EditingButtonCard.svelte
+++ src/lib/cards/ButtonCard/EditingButtonCard.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
 	import type { ContentComponentProps } from '../types';
 	import PlainTextEditor from '$lib/components/PlainTextEditor.svelte';
+	import { cn } from '@foxui/core';
 
 	let { item = $bindable() }: ContentComponentProps = $props();
 </script>
 
-<div class="text-base-950 dark:text-base-50 flex h-full w-full flex-col items-center justify-center gap-2 px-4">
+<div
+	class="text-base-950 dark:text-base-50 flex h-full w-full flex-col items-center justify-center gap-2 px-4"
+>
 	<PlainTextEditor
 		key="text"
 		bind:contentDict={item.cardData}
 		placeholder="Button text"
-		class="line-clamp-1 text-center text-2xl font-bold"
+		class={cn(
+			'line-clamp-1 text-center text-2xl font-semibold',
+			item.color === 'transparent'
+				? 'bg-accent-500 dark:text-base-950 rounded-2xl px-5 py-2.5 text-xl'
+				: ''
+		)}
 	/>
 </div>
