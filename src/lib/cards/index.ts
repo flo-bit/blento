37aab1b876c1e561cfeede58e9a69066589e85c2
unbedenklich <106080544+unbedenklich@users.noreply.github.com>
2026-01-31 23:30:15 +0100

    copy pages and add allcards to page in dev mode

--- src/lib/cards/index.ts
+++ src/lib/cards/index.ts
@@ -49,7 +49,7 @@ export const AllCardDefinitions = [
 	LatestBlueskyPostCardDefinition,
 	LivestreamCardDefitition,
 	LivestreamEmbedCardDefitition,
-	EmbedCardDefinition,
+	// EmbedCardDefinition,
 	MapCardDefinition,
 	ATProtoCollectionsCardDefinition,
 	SectionCardDefinition,
