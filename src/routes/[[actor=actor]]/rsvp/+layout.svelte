8c9ce842d54e3637014740c3890309ac60cd1279
Florian <45694132+flo-bit@users.noreply.github.com>
2026-04-09 00:38:35 +0200

    remove /events and /rsvp

--- src/routes/[[actor=actor]]/rsvp/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import ThemeScript from '$lib/website/ThemeScript.svelte';
-
-	let { data, children } = $props();
-</script>
-
-<ThemeScript accentColor={data.accentColor} baseColor={data.baseColor} />
-
-{@render children()}
